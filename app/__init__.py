5ed677125fdabbd6ca6d06eae66a338c60aa8789
Anen0 <Lum3nGee@gmail.com>
2025-12-04 11:44:23 +0800

    12/4/2025 update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from dotenv import load_dotenv
 import pymysql
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -10,8 +15,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://testman:Jhayg3309]]:P@
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
+# - keys for managing user sessions
+login_manager = LoginManager(app)   # - validation for user session 
+login_manager.login_view = 'login'   # - login end point  @login_rquired will redirect to 'login' page
+login_manager.login_message_category = 'info'      
 
 from app import routes
