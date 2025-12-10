03e1da7f3502da606c5114a28b9f0b286d33a7b7
Anen0 <Lum3nGee@gmail.com>
2025-12-10 16:10:02 +0800

    Add requirements.txt and secure config with environment variables
    - Added requirements.txt with all Python dependencies (Flask, SQLAlchemy, etc.)
    - Removed hardcoded credentials from config.py
    - Added .env.example template for environment variables
    - Updated .gitignore to protect .env and other sensitive files
    - Project now uses Python 3.14 with all dependencies properly managed

--- app/config.py
+++ app/config.py
@@ -16,11 +16,4 @@ class Config:
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
 
-class Config:
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://testman:Jhayg3309]]:P@vultr-prod-85f8d360-5bbf-4d05-ad2d-01cc47768728-vultr-prod-995c.vultrdb.com:16751/sample_crud'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'pgeno***@gmail.com')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '') 
+ 
